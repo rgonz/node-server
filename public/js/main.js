ed2aa3537a8ab7e32fedf26f0d90e8bf4ca07da4
rgonz <rafael.gonzalez@ieee.org>
2015-09-07 21:32:49 -0400

    Day 4 Part 2: Added Calendar App

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+/* Javascript */
+$(function () {
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
+
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		editable: true
+	});
+});
